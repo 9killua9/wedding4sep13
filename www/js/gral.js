60882cccbe49432d8633aaccfee9a05e08d897b6
9killua9 <leandro.salar@gmail.com>
2013-09-15 19:23:11 -0300

    5- testeo del login terminado y anim del DSHBRD

--- www/js/gral.js
+++ www/js/gral.js
@@ -2,7 +2,7 @@
 
 /* Declaracion de variables
 ============================ */
-$url     = 'php/funciones.php';
+$url     = 'http://www.reiatsu.com.ar/clientes/juliocaunedo/phonegap/wedding/php/funciones.php';
 $urlFbk  = 'php/facebook/facebookphp.php';
 $nImagen = 0;
 $w       = parseInt($(window).width());
@@ -13,29 +13,57 @@ $h       = parseInt($(window).height());
 $(document).ready(function(){
     /* clase .nada hace que no se envien datos ni funcionen clicks 
 ==================================================================== */
-    $(".nada").click(function(a){
+$(".nada").click(function(a){
     	a.preventDefault();
     }).submit(function(a){
     	a.preventDefault();
     });     
+
+    cargaTotal();
 });
 
-/* Comienzo de las funciones 
-=================================================================== */
-function funcionesDeCarga() // carga esta funcion cuando termina de cargar el 
+function cargaTotal()
 {
-    $("#ideaDeFondo").css({width:$w,height:$h});
+    /*Acomoda tam de dashboard*/
+    $(".dashboardCubos").css({width:(($w/2)-1), height:(($h-95)/3)})
+    /*Dirige seccion al cargar*/
+    var $ur = window.localStorage.getItem("ur");
+    if($ur != null)
+        cambiaSeccion("pageMenu");
+    else
+        cambiaSeccion("pageLogin");
+    
+    /* Login y usuarios */
+    $("form[name=login]").submit(function(a){
+        a.preventDefault();
+        if( $("form[name=login] input[name=user]").val().length > 4 && $("form[name=login] input[name=password]").val() != "" )
+        {
+            $data = 'h=login&'+$("form[name=login]").serialize();
+            lmPost($url,$data,"login");
+        }
+        else
+            alert("Complete Nick and Password");
 
-    // arma el fondo
-    $k=15;
-    $i=0;
-    for($i=0;$i<$k;$i++)
-    {
-        $t = $w/($i+1);
-        $("#ideaDeFondo").append('<div class="redondito positionAbsolute medida'+$i+'" style="width:'+$t+'px;height:'+$t+'px"></div> ');
-    } //fin del bucle que hace el fondo
-} // fin de funcionesDeCarga
-
+        return false;
+    });
+}
+function cambiaSeccion(page)
+{    
+    $(".medidasPaginas").addClass("animacionFadeCenterScaleOut").addClass("one");
+    setTimeout(function(){
+        $("#"+page).removeClass("animacionFadeCenterScaleOut").removeClass("oculto").addClass("animacionFadeCenterScale").addClass("one");
+        switch(page)
+        {
+            case "pageMenu":
+                setTimeout(function(){
+                    $(".dashboardCubos").each(function(i){
+                        $(this).css("-webkit-animation-delay", i+".1s").removeClass("oculto").addClass("animacionFadeCenterScale");
+                    });        
+                },600);
+            break;
+        }
+    },700);
+}
 /* Funciones de el framework para leer los resultados del ajax
 =================================================================== */
 function termina(xq,v)
@@ -44,9 +72,14 @@ function termina(xq,v)
      * xq -> corresponde al caso del switch
      * v -> resultado en array que envia el ajax
      */
-    if( xq == "enviamail" )
+    if( xq == "login" )
     {
-        $("form[name=solicitud] input[type=text]").val("");
-        $("form[name=solicitud] input[name=nombre]").val("Muchas gracias");       
+        if(v['aviso'] == "si")
+        {
+            window.localStorage.setItem("ur", v['nick']);
+            window.localStorage.setItem("dispositivo", v['dispositivo']);
+
+            cambiaSeccion("pageMenu");
+        }
     }
 }
